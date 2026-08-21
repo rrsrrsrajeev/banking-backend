e01d7c21a5e34d67cb53a1dfa7e9522b1b5c4863
Rajeev Singh <rrsrrsrajeev@gmail.com>
2026-08-21 15:35:25 +0530

    feat: initialize Spring Boot backend project with Maven wrapper and basic configuration

---
+++ src/test/java/com/abhay/banking_backend/BankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhay.banking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
